994d9a2ee5489fe923112adc2018eb37b4969b5f
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2021-11-25 21:21:29 +0200

    Refactor to functional components with hooks(useState hook)

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,20 +1,17 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
-export default class Output extends React.Component {
-  render() {
-    const { total, next, operation } = this.props;
-    return (
-      <div className="output">
-        <div className="operand">
-          <p>
-            { total }
-            { operation }
-            { next }
-          </p>
-        </div>
+export default function Output(props) {
+  const { total, next, operation } = props;
+  return (
+    <div className="output">
+      <div className="operand">
+        <p>
+          { total }
+          { operation }
+          { next }
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
