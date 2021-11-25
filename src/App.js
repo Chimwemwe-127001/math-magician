21881d779bdca273ef89f092a06e16e5d43dba7c
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2021-11-25 18:08:19 +0200

    Lift state up and pass props to child components as requested by reviewer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import Calculator from './components/Calculator';
+import calculate from './logic/calculate';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+    state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+
+  clickHandler = (event) => {
+    this.setState((initialState) => calculate(initialState, event));
   }
 
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Calculator
+          total={this.state.total}
+          next={this.state.next}
+          operation={this.state.operation}
+          clickHandler={this.clickHandler}
+        />
       </div>
     );
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,55 +1,24 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import '../index.css';
-import calculate from '../logic/calculate';
+import Buttons from './Buttons';
+import Output from './Output';
 
-// eslint-disable-next-line react/prefer-stateless-function
+// eslint-disable react/prefer-stateless-function
 export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: null,
-      operation: null,
-    };
-    this.clickHandler = this.clickHandler.bind(this);
-  }
-
-  clickHandler = (event) => {
-    this.setState((initialState) => calculate(initialState, event));
-  }
-
   render() {
-    const { total, next, operation } = this.state;
     return (
       <div className="calculator-grid">
-        <div className="output">
-          <div className="operand">
-            <p>
-              { total }
-              { operation }
-              { next }
-            </p>
-          </div>
-        </div>
-        <button type="button" onClick={() => this.clickHandler('AC')}>AC</button>
-        <button type="button" onClick={() => this.clickHandler('+/-')}>+/-</button>
-        <button type="button" onClick={() => this.clickHandler('%')}>%</button>
-        <button type="button" className="orange" onClick={() => this.clickHandler('÷')}>÷</button>
-        <button type="button" onClick={() => this.clickHandler('7')}>7</button>
-        <button type="button" onClick={() => this.clickHandler('8')}>8</button>
-        <button type="button" onClick={() => this.clickHandler('9')}>9</button>
-        <button type="button" className="orange" onClick={() => this.clickHandler('x')}>x</button>
-        <button type="button" onClick={() => this.clickHandler('4')}>4</button>
-        <button type="button" onClick={() => this.clickHandler('5')}>5</button>
-        <button type="button" onClick={() => this.clickHandler('6')}>6</button>
-        <button type="button" className="orange" onClick={() => this.clickHandler('-')}>-</button>
-        <button type="button" onClick={() => this.clickHandler('1')}>1</button>
-        <button type="button" onClick={() => this.clickHandler('2')}>2</button>
-        <button type="button" onClick={() => this.clickHandler('3')}>3</button>
-        <button type="button" className="orange" onClick={() => this.clickHandler('+')}>+</button>
-        <button type="button" className="span_two" onClick={() => this.clickHandler('0')}>0</button>
-        <button type="button" onClick={() => this.clickHandler('.')}>.</button>
-        <button type="button" className="orange" onClick={() => this.clickHandler('=')}>=</button>
+        <Output
+          total={this.props.total}
+          operation={this.props.operation}
+          next={this.props.next}
+        />
+        <Buttons
+          clickHandler={this.props.clickHandler}
+        />
       </div>
     );
   }
