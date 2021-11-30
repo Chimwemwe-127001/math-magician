40e199436ec9ce9b2a3fabb9a1ddc4010e7a56d3
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2021-11-30 17:57:39 +0200

    Add hover animation to nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navigation">
-    <h1>Math Magicians</h1>
+    <h1><Link className="title" to="/">Math Magicians</Link></h1>
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/calculator">Calculator</Link></li>
